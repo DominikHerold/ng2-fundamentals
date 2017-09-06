8cd96646f5b1d919f27cadb11a7fa57dc5b65d44
Dominik Herold <d.herold@cid.biz>
2017-09-06 15:41:54 +0200

    add styles only for components

--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -2,7 +2,10 @@ import {Component} from '@angular/core'
 
 @Component({
     selector: 'events-list',
-    templateUrl: 'app/events/events-list.component.html'
+    templateUrl: 'app/events/events-list.component.html',
+    styles: [`    
+    .well div { color: red;}
+`] 
 })
 export class EventListComponent{
 event = {
