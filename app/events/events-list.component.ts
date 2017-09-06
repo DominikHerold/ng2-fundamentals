2ff98cac12fec16318a637f7f93d4f9a850526f8
Dominik Herold <d.herold@cid.biz>
2017-09-06 14:13:51 +0200

    add template

--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -2,20 +2,12 @@ import {Component} from '@angular/core'
 
 @Component({
     selector: 'events-list',
-    template: `
-    <div>
-        <h1>Upcoming Events</h1>
-        <hr/>
-        <div class="well hoverwell thumbnail">
-            <h2>{{event.name}}</h2>
-            <div>ID: {{event.id}}</div>
-        </div>        
-    </div>
-    `
+    templateUrl: 'app/events/events-list.component.html'
 })
 export class EventListComponent{
 event = {
     id: 1,
-    name: 'MyEvent'
+    name: 'MyEvent',
+    cost: 567.89
 }
 }
