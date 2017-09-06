5963bdc7bab319bebea7cedfebd635aa8af0b8e6
Dominik Herold <d.herold@cid.biz>
2017-09-06 15:28:00 +0200

    add styles

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -7,9 +7,17 @@ import {Component, Input, Output, EventEmitter} from '@angular/core'
         <h2>{{event.name}}</h2>
         <div>ID: {{event.id}}</div>
         <div>Cost: {{event.cost}} $</div>
+        <div>
+            <span>foo</span>
+            <span class="pad-left">bar</span>
+        </div>
         <button class="btn btn-primary" (click)="handleClickMe()">Klicken</button>
     </div> 
-    `
+    `,
+    styles: [`
+        .pad-left { margin-left: 50px;}
+        .well div { color: #bbb;}
+    `]  
 })
 export class EventThumbnailComponent{
     @Input() event:any
